4dad7086bff8cfd20020b7dea805fcca2b2a2976
Mohamed Abdessmad <mabdessm@c2r8p2.42nice.fr>
2024-05-08 19:14:31 +0200

    First commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 19:07:50 by mabdessm          #+#    #+#             */
+/*   Updated: 2024/05/08 19:11:56 by mabdessm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
