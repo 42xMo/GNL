99518f738898ad00ebed3a099f3fe9526fa025b3
Mohamed Abdessmad <mabdessm@c2r8p2.42nice.fr>
2024-05-11 17:26:24 +0200

    gnl changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 19:07:54 by mabdessm          #+#    #+#             */
-/*   Updated: 2024/05/11 17:22:32 by mabdessm         ###   ########.fr       */
+/*   Updated: 2024/05/11 17:26:03 by mabdessm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ char	*get_next_line(int fd)
 {
 	char		*buffer;
 	char		*res;
-	static char	*static_line;
+	static char	*static_res;
 	
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
-		return (0);
+		return (NULL);
 	res = //function to get next line;
 	free(buffer);
 	if (!res)
-		return (0);
-	static_line = //function to keep the static var;
+		return (NULL);
+	static_res = //function to keep the static line;
 	return (res);
 }
 int	main(int argc, char **argv)
